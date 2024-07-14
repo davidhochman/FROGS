1cbdf19dad947e058bd913ffcb516af407082494
AthenaWrenn <162925464+AthenaWrenn@users.noreply.github.com>
2024-07-14 10:56:31 -0400

    Update App.js
    Edited the start message

--- frogs-frontend/src/App.js
+++ frogs-frontend/src/App.js
@@ -14,6 +14,7 @@ function App() {
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <p>
+                        You sucessfully loaded the project!
                         Edit <code>src/App.js</code> and save to reload.
                     </p>
                     <a
